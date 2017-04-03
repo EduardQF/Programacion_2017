70384071cc8f9a7c24187f59bbcad92d0b3b715f
Eduardo Queupumil F <e.queupumil01@ufromail.cl>
2017-04-03 14:28:03 -0300

    Add files via upload

---
+++ video3/App.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package video3;
+
+/**
+ *
+ * @author Eduard QF
+ */
+public class App {
+
+    public static void main(String[] args) {
+        /*Alumno.sumar(0);*/
+        Alumno al = new Alumno();
+        System.out.println(al.sumar(0));
+    }
+
+}
---
+++ video3/Persona.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package video3;
+
+/**
+ *
+ * @author Eduard QF
+ */
+public class Persona {
+
+    /*public int sumar(int numero){
+        return numero + 1;
+    }*/
+    public int sumar(int numero) {
+        return numero + 1;
+    }
+}
