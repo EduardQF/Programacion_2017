aceb0f001c252d1fd11b7deb5b8a5208f81fcd9f
Eduardo Queupumil F <e.queupumil01@ufromail.cl>
2017-04-04 09:23:52 -0300

    Update and rename MitoCode.java to Conexion.java

---
+++ video5/Conexion.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package video5;
+
+/**
+ *
+ * @author Eduard QF
+ */
+public class Conexion {
+    private static Conexion instancia =null;
+    
+    public static Conexion getInstance(){
+        if(instancia==null){
+            instancia=new Conexion();
+        }
+        return instancia;
+    }
+    
+    private Coneccion(){
+       
+       }
+}
